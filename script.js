b05309ed596334f8da3bd6667901383fbeb27872
Saba Bolkvadze <sababolqvadze54@gmail.com>
2024-01-11 19:57:08 +0400

    first commit for lecture 30

---
+++ script.js
@@ -0,0 +1,84 @@
+// var pirveliCifri = 15;
+// var meoreCifri = 4;
+// var jami = pirveliCifri + meoreCifri;
+
+// console.log(jami);
+
+// document.getElementById("jami").innerHTML = jami
+
+// document.getElementsByClassName("demo")[2].innerHTML = jami
+
+let mimateba1 = 32;
+let plus = "+";
+let mimateba2 = 14;
+let mimatebaJami = mimateba1 + mimateba2;
+
+document.getElementById("mimateba1").innerHTML = mimateba1;
+document.getElementById("plus").innerHTML = plus;
+document.getElementById("mimateba2").innerHTML = mimateba2;
+document.getElementById("mimatebaJami").innerHTML = mimatebaJami;
+
+let gamokleba1 = 67;
+let minusi = "-";
+let gamokleba2 = 24;
+let gamoklebaPasuxi = gamokleba1 - gamokleba2;
+
+document.getElementById("gamokleba1").innerHTML = gamokleba1;
+document.getElementById("minusi").innerHTML = minusi;
+document.getElementById("gamokleba2").innerHTML = gamokleba2;
+document.getElementById("gamoklebaPasuxi").innerHTML = gamoklebaPasuxi;
+
+
+let samravli = 67;
+let gamravleba = "*";
+let mamravli = 24;
+let namravli = samravli * mamravli;
+
+document.getElementById("samravli").innerHTML = samravli;
+document.getElementById("gamravleba").innerHTML = gamravleba;
+document.getElementById("mamravli").innerHTML = mamravli;
+document.getElementById("namravli").innerHTML = namravli;
+
+
+let asayvaniRicxvi = 4;
+let xarisxisNishani = "^"
+let xarisxi = 6;
+let pasuxi = asayvaniRicxvi ** xarisxi;
+
+document.getElementById("asayvaniRicxvi").innerHTML = asayvaniRicxvi;
+document.getElementById("xarisxisNishani").innerHTML = xarisxisNishani;
+document.getElementById("xarisxi").innerHTML = xarisxi;
+document.getElementById("pasuxi").innerHTML = pasuxi;
+
+let fudze = 125;
+let fesvisNishani = "?";
+let fesvisXarisxi = 3;
+let pasuxiFesvi = Math.cbrt(fudze);
+;
+document.getElementById("fudze").innerHTML = fudze
+document.getElementById("fesvisNishani").innerHTML = fesvisNishani;
+document.getElementById("fesvisXarisxi").innerHTML = fesvisXarisxi;
+document.getElementById("pasuxiFesvi").innerHTML = pasuxiFesvi;
+
+
+
+let gayofa1 = 10;
+let gayofisNishani = "/"
+let gayofa2 = 5;
+let pasuxiGayofa = gayofa1 / gayofa2;
+
+document.getElementById("gayofa1").innerHTML = gayofa1;
+document.getElementById("gayofisNishani").innerHTML = gayofisNishani;
+document.getElementById("gayofa2").innerHTML = gayofa2;
+document.getElementById("pasuxiGayofa").innerHTML = pasuxiGayofa;
+
+
+
+
+
+
+
+
+
+
+
